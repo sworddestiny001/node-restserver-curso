1f023eb079fd1209cfa5a4b3ef789736b3549338
Jesus P Mac <sword.destiny.001@gmail.com>
2020-06-19 22:55:43 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://mongoUser:Gsm.sky.0@cluster0-otpnl.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
